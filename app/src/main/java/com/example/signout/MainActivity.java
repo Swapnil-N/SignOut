0af5c5166cd17e78e076e6095dda3e9a77e3d920
Swapnil N <swapnil.napuri@gmail.com>
2019-02-16 03:24:57 -0500

    Finished basic set up

---
+++ app/src/main/java/com/example/signout/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.signout;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new CameraFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment frag = null;
+
+        switch (item.getItemId()){
+
+            case R.id.navigation_camera:
+                frag = new CameraFragment();
+                break;
+
+            case R.id.navigation_text:
+                frag = new TextFragment();
+                break;
+        }
+        return loadFragment(frag);
+
+    }
+}
