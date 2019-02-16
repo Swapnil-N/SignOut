641d3a343af784337f7d557b023c73b3375d4f22
Sid <tnstjoshi@gmail.com>
2019-02-16 14:31:07 -0500

    -added camera functionality

--- app/src/main/java/com/example/signout/CameraFragment.java
+++ app/src/main/java/com/example/signout/CameraFragment.java
@@ -1,6 +1,9 @@
 package com.example.signout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
@@ -8,15 +11,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.VideoView;
+
+import static android.app.Activity.RESULT_OK;
 
 public class CameraFragment extends Fragment {
 
+    VideoView videoView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_camera, null);
 
+        videoView=view.findViewById(R.id.ID_videoView);
+        dispatchTakeVideoIntent();
+
         return view;
+    }
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+        }
     }
 }
--- app/src/main/java/com/example/signout/TextFragment.java
+++ app/src/main/java/com/example/signout/TextFragment.java
@@ -1,14 +1,21 @@
 package com.example.signout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.VideoView;
 
 public class TextFragment extends Fragment {
+
+    VideoView videoView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -17,4 +24,5 @@ public class TextFragment extends Fragment {
 
         return view;
     }
+
 }
