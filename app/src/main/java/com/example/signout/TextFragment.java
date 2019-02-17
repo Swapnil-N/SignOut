1f04caba6a80e57b9eaea0ea2ead1cc3feb0275c
Swapnil N <swapnil.napuri@gmail.com>
2019-02-17 11:50:58 -0500

    -Added text to speech

--- app/src/main/java/com/example/signout/TextFragment.java
+++ app/src/main/java/com/example/signout/TextFragment.java
@@ -11,10 +11,18 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 //import com.ibm.watson.developer_cloud.service.security.IamOptions;
 
 public class TextFragment extends Fragment {
+
+    TextToSpeech tts;
+    EditText editText;
+    Button speakButton;
 
 
     @Nullable
@@ -22,6 +30,26 @@ public class TextFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_text, null);
+
+        editText = view.findViewById(R.id.editText);
+        speakButton = view.findViewById(R.id.ID_SpeakButton);
+
+        tts = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR){
+                    tts.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toSpeak = editText.getText().toString();
+                tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
 
         return view;
     }
