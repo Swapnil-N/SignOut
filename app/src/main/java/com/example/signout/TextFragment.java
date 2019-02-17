41c2ac94fc8c4478eb2ef2b08ceede23b93ab2ed
Swapnil N <swapnil.napuri@gmail.com>
2019-02-17 09:36:33 -0500

    more Code cleanup

--- app/src/main/java/com/example/signout/TextFragment.java
+++ app/src/main/java/com/example/signout/TextFragment.java
@@ -11,13 +11,11 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.VideoView;
 
-import com.ibm.watson.developer_cloud.service.security.IamOptions;
+//import com.ibm.watson.developer_cloud.service.security.IamOptions;
 
 public class TextFragment extends Fragment {
 
-    VideoView videoView;
 
     @Nullable
     @Override
